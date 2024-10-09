de20dd2ea41b05654fb3812bdaa033f0b8988d58
Fardeen Khan <159772589+Fardeennnnnnnnnnkhan@users.noreply.github.com>
2024-10-09 23:48:41 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -58,7 +58,7 @@ function Works() {
 <div className='w-full flex'>
   <motion.p className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px] '   variants={fadeIn("" , "" , 0.1 , 1)}>
 
-    Follwoing porjects showcases my skills and experience through real-world examples of my work . Each project is briefly described  with links to code repositories and live demos in it . It reflects my ability to solve complex problems , work with different technologies , manage projects effectively . 
+    Following projects showcases my skills and experience through real-world examples of my work . Each project is briefly described  with links to code repositories and live demos in it . It reflects my ability to solve complex problems , work with different technologies , manage projects effectively . 
   </motion.p>
 </div>
 
